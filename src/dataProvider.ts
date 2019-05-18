5ebd44b212946337e95ae75b6c8ee32e9641a690
AKosmachyov <a.kosmachyov@gmail.com>
2019-05-18 18:51:52 +0300

    Add mongoose models

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,5 +1,5 @@
-interface DataProvider {
-    init(): Promise<void>;
+export interface DataProvider {
+    init(): Promise<any>;
 
     addChat(options: { telegramId: number, title: string, chatType: string }): Promise<Chat>;
     getChat(id: number): Promise<Chat>;
@@ -15,7 +15,7 @@ interface DataProvider {
     addOrUpdateAnswer(poll: Poll, user: User, answer: string): Promise<PollAnswer>;
 }
 
-interface Chat {
+export interface Chat {
 	id: number;
 	telegramId: number;
 	title: string;
@@ -23,14 +23,14 @@ interface Chat {
 	users: User[];
 }
 
-interface User {
+export interface User {
 	id: number;
 	telegramId: number;
 	firstName: string;
 	lastName: string;
 }
 
-interface Poll {
+export interface Poll {
 	id: number;
 	chat: Chat;
 	title: string;
@@ -42,12 +42,12 @@ interface Poll {
 
 type PollValue = string;
 
-interface PollOption {
+export interface PollOption {
 	title: string;
 	value: PollValue;
 }
 
-interface PollAnswer {
+export interface PollAnswer {
 	id: number;
 	user: User;
 	answer: PollValue;
---
+++ src/mongoose/PollModel.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+const PollSchema = new Schema({
+	chat: { type: Schema.Types.ObjectId, ref: 'Chat' },
+	title: String,
+	createDate: Date,
+	endDate: Date,
+
+	options: [
+		{
+			title: String,
+			value: String
+		}
+	],
+	answers: [
+		{
+			user: { type: Schema.Types.ObjectId, ref: 'User' },
+			answer: String
+		}
+	]
+});
+
+const PollModel = model('Poll', PollSchema);
+
+export default PollModel;
