0f70fcbaa0ccb9c9e071b4e1c72796ddc7433b4e
AKosmachyov <a.kosmachyov@gmail.com>
2019-05-18 18:15:32 +0300

    Add interface for data provider

---
+++ src/dataProvider.ts
@@ -0,0 +1,54 @@
+interface DataProvider {
+    init(): Promise<void>;
+
+    addChat(options: { telegramId: number, title: string, chatType: string }): Promise<Chat>;
+    getChat(id: number): Promise<Chat>;
+    getChatForUser(userId: number): Promise<Chat[]>;
+    addUserForChat(id: number, user: User): Promise<void>;
+    
+    getUser(telegramId: number): Promise<User>;
+    addOrUpdateUser(user: User): Promise<void>;
+    
+    addPoll(poll: Poll): Promise<Poll>;
+    getPoll(id: number): Promise<Poll>;
+    getActivePollsForChat(id: number): Promise<Poll>;
+    addOrUpdateAnswer(poll: Poll, user: User, answer: string): Promise<PollAnswer>;
+}
+
+interface Chat {
+	id: number;
+	telegramId: number;
+	title: string;
+	chatType: string;
+	users: User[];
+}
+
+interface User {
+	id: number;
+	telegramId: number;
+	firstName: string;
+	lastName: string;
+}
+
+interface Poll {
+	id: number;
+	chat: Chat;
+	title: string;
+	createDate: Date;
+    endDate: Date;
+    pollOptions: PollOption[];
+    answers: PollAnswer[];
+}
+
+type PollValue = string;
+
+interface PollOption {
+	title: string;
+	value: PollValue;
+}
+
+interface PollAnswer {
+	id: number;
+	user: User;
+	answer: PollValue;
+}
